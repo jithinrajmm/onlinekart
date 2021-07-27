8ea21e1e61a2f8a1af0f4a578265fe299cb04153
jithin raj <jithinbuasc@gmail.com>
2021-07-27 06:34:49 +0530

    debug mode is set as false and ALLOWED_HOST=['*']

--- ikart/ikart/settings.py
+++ ikart/ikart/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n8od*^!m1xrt&9b@abew-+cux+_vajtb!ouqras_328^qya%e$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
